fad9faeeb0640c6756fda2078c4b9258fb058c2e
Kadir Berk Yağar <ykadirberk2@gmail.com>
2024-09-01 16:53:33 +0300

    fix: Move is aligned with std

--- include/pufd/Utils/Move.h
+++ include/pufd/Utils/Move.h
@@ -7,10 +7,29 @@
 
 namespace pufd
 {
+	// cppreference implementation of move semantics
+
+	template<class T> struct remove_reference { typedef T type; };
+	template<class T> struct remove_reference<T&> { typedef T type; };
+	template<class T> struct remove_reference<T&&> { typedef T type; };
+
+
 	template <typename Type>
-	auto move(Type& type) -> Type&&
+	constexpr auto move(Type&& type) -> typename remove_reference<Type>::type&&
 	{
-		return static_cast<Type&&>(type);
+		return static_cast<typename std::remove_reference<Type>::type&&>(type);
+	}
+
+	template <typename Type>
+	constexpr auto forward(typename remove_reference<Type>::type& type) noexcept -> Type&&
+	{
+		return static_cast<typename remove_reference<Type>::type&>(type);
+	}
+
+	template <typename Type>
+	constexpr auto forward(typename remove_reference<Type>::type&& type) noexcept -> Type&&
+	{
+		return static_cast<typename remove_reference<Type>::type&&>(type);
 	}
 }
 
